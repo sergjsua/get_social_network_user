6249df81d53c84127da9ad9a497cb79dc8473064
sergjsua <sergjs.ua@gmail.com>
2018-11-08 19:23:16 +0200

    Add user's posts view

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -7,6 +7,6 @@ module.exports = {
 
   render: (request, response) => {
 		return response.view('homepage')
-  },
+  }
 
 }
--- api/services/DataParser.js
+++ api/services/DataParser.js
@@ -6,7 +6,7 @@ let UserInstagramModel = require('../models/UserInstagramModel.js').UserInstagra
 
 module.exports = {
 	
-	parseInstagramData: function(source) {
+	parseInstagramData: function (source) {
 		const prefix = '<script type="text/javascript">window._sharedData = '
 		const suffix = ';</script>'
 		const notFoundMessage = 'The link you followed may be broken, or the page may have been removed'
@@ -14,14 +14,16 @@ module.exports = {
 		if(!userJSON) {
 			return false
 		}
-
 		const userData = JSON.parse(userJSON).entry_data.ProfilePage[0].graphql.user
+		const posts = userData.edge_owner_to_timeline_media.edges
 		const userObject = new UserInstagramModel(
 			userData.full_name,
 			userData.biography,
 			userData.edge_followed_by.count,
 			userData.edge_follow.count
 		)
+		
+		userObject.posts = this.getLastPosts(posts)
 
 		return userObject
 	},
@@ -50,6 +52,23 @@ module.exports = {
 			}
 		}
 		return source
+	}, 
+
+	getLastPosts: function (posts) {
+		let resultPostsData = []
+
+		if (posts.length > 0) {
+			for (let i = 0; posts.length > i && i < 6; i++) {
+				let post = posts[i].node
+
+				resultPostsData[i] = {
+					id: post.id,
+					imageUrl: post.display_url
+				}
+			}
+		}
+
+		return resultPostsData
 	}
 
 };
